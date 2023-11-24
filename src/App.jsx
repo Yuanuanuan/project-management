957fe6b07d5db26e409d2cd7061ef3045174ea4b
Yuanuanuan <asdll6618@gmail.com>
2023-11-24 15:21:14 +0800

    feat: NewProject & Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSibeBar from './components/ProjectsSibebar';
+import NewProject from './components/NewProject';
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSibeBar />
+      <NewProject />
     </main>
   );
 }
