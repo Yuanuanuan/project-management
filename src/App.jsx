89be6b6837f936901c7cc605425dfad9e479a61a
Yuanuanuan <asdll6618@gmail.com>
2023-11-24 15:55:14 +0800

    feat: add state to manage want to show NewProject component or NoProjectSelect component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import React from "react";
+
 import ProjectsSibeBar from "./components/ProjectsSibebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = React.useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSibeBar />
-      <NoProjectSelected />
+      <ProjectsSibeBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
