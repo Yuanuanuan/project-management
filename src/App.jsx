b4a5838e83b43b6a5e4aed1ef684fc4ff5216a8d
Yuanuanuan <asdll6618@gmail.com>
2023-11-24 15:38:10 +0800

    feat: NoProjectSelected component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import ProjectsSibeBar from './components/ProjectsSibebar';
-import NewProject from './components/NewProject';
+import ProjectsSibeBar from "./components/ProjectsSibebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSibeBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
